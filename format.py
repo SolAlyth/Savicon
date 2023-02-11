04075cb15e741e80e933991ea7ddf3b137bfb6a0
SolAlyth <sol.nkj.rall@gmail.com>
2023-02-11 16:43:08 +0900

    debug の追加

--- format.py
+++ format.py
@@ -1,16 +1,54 @@
-# from re import sub
-
-with open("./target/main.js", encoding="utf8") as main_inpf:
-    maint = main_inpf.read()
-
-with open("./docs/main.js", "w", encoding="utf8") as main_outf:
-    # main_outf.write( sub("\n *", " ", maint)[:-1].replace('from "./lib"', 'from "./lib.js"') )
-    main_outf.write(maint.replace('from "./lib"', 'from "./lib.js"'))
+from re import sub, findall
+from subprocess import run as subprocess_run
 
 
-with open("./target/lib.js", encoding="utf8") as lib_inpf:
-    libt = lib_inpf.read()
+# !debug が付いた行を削除して src/tmp にコピー
 
-with open("./docs/lib.js", "w", encoding="utf8") as lib_outf:
-    # lib_outf.write( sub("\n *", " ", libt)[:-1] )
-    lib_outf.write(libt)
+debug_regexp = "\n *(.*?) *// !debug.*"
+
+def delete_debug_and_copy(filename):
+    with open(f"./src/{filename}.ts", encoding="utf-8") as tsf:
+        ts = tsf.read()
+    with open(f"./src/tmp/{filename}.ts", "w", encoding="utf-8") as tmpf:
+        delete_lines = findall(debug_regexp, ts)
+        print(f"{filename}: find {len(delete_lines)} lines ... {delete_lines}")
+        
+        tmpf.write(
+            sub(debug_regexp, "", ts)
+        )
+
+print("debug lines:")
+[[print("    ", end=""), delete_debug_and_copy(filename)] for filename in ["main", "lib"]]
+print("")
+
+
+# tsc の実行
+# なぜか flush を指定しないと表示されない
+print("tsc: ", end="", flush=True)
+subprocess_run("tsc", shell=True)
+print("end")
+
+
+
+# import の修正, 改行の削除, 末尾の空白の削除をしてから docs にコピー
+# target/tmp -> docs, target -> docs/debug に注意
+
+def fix_and_copy(target_path, docs_path):
+    with open(f"./target/{target_path}.js", encoding="utf-8") as jsf:
+        js = jsf.read()
+    with open(f"./docs/{docs_path}.js", "w", encoding="utf-8") as docsf:
+        js = js.replace('from "./lib"', 'from "./lib.js"')
+        js = sub("\n *", " ", js)
+        if js[-1] == " ":
+            js = js[:-1]
+        
+        docsf.write(js)
+
+[ fix_and_copy(target, docs) for (target, docs) in
+    [
+        ["tmp/main", "main"],
+        ["tmp/lib", "lib"],
+        ["main", "debug/main"],
+        ["lib", "debug/lib"]
+    ]
+]
